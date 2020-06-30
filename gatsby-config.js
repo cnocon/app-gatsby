ef70aa0d59b10f84223c07ab51e69dfe90c5c4af
Cristin O'Connor <cristin@MacBook-Pro.local>
2020-06-29 20:06:16 -0600

    Removed extra image

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/cristin-io-favicon.png`,
+        icon: `src/images/square-portrait.png`,
       },
     },
     {
